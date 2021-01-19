00d3793df2db1cbb173e12da897ecf8d7df42350
Thato Mmusi <mmusi2010@gmail.com>
2021-01-19 22:37:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,30 @@
-// in main.js
-function loadContent(){
-  var contentDiv = document.getElementById("app"),
+function getContent(fragmentId, callback){
 
-      // This gets rid of the first character of the string
+  var pages = {
+    home: "This is the Home page. Welcome to my site.",
+    about: "This page will describe what my site is about",
+    contact: "Contact me on this page if you have any questions"
+  };
+
+  callback(pages[fragmentId]);
+}
+
+
+
+function loadContent(){
+
+  var contentDiv = document.getElementById("app"),
       fragmentId = location.hash.substr(1);
 
-  contentDiv.innerHTML = fragmentId;
+  getContent(fragmentId, function (content) {
+    contentDiv.innerHTML = content;
+  });
+
 }
 
 if(!location.hash) {
   location.hash = "#home";
 }
-
 
 loadContent();
 
