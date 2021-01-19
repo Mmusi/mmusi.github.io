292509874345cd6d920a1802151a8acc0d4c97f5
Thato Mmusi <mmusi2010@gmail.com>
2021-01-19 22:36:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 // in main.js
 function loadContent(){
-  var contentDiv = document.getElementById("app");
-  contentDiv.innerHTML = location.hash;
+  var contentDiv = document.getElementById("app"),
+
+      // This gets rid of the first character of the string
+      fragmentId = location.hash.substr(1);
+
+  contentDiv.innerHTML = fragmentId;
 }
 
-window.addEventListener("hashchange", function (){
-  loadContent();
-});
+if(!location.hash) {
+  location.hash = "#home";
+}
+
 
 loadContent();
+
+window.addEventListener("hashchange", loadContent)
